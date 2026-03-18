44adff9361d4877e0f9b1616d26e1b49879c345f
teohome <64443357+teohome@users.noreply.github.com>
2026-03-18 16:12:15 -0300

    Delete blackbox directory

--- blackbox/util/genbb.bat
+++
@@ -1,6 +0,0 @@
-rem 
-rem  Se puede DISTRIBUIR 
-rem 
-rem bacht para generar caja negra IDPAS
-rem 
-genbbset W . bbtest bbtest test01..
--- blackbox/util/inspeccionar_bb.bat
+++
@@ -1,6 +0,0 @@
-rem 
-rem  NO DISTRIBUIR !!!!
-rem 
-rem bacht para inspeccionar caja negra IDPAS
-rem 
-genbbset I ./ bbtest
